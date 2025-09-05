95702f0251f6c583c130f9088634332559372b80
LuisGT84 <lateworks21@gmail.com>
2025-09-05 01:13:10 -0600

    S2: componente hijo TodoItem (checkbox, texto, eliminar)

---
+++ src/components/todos/TodoItem.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  todo: { type:Object, required:true }
+})
+const emit = defineEmits(['toggle','remove'])
+</script>
+
+<template>
+  <li class="item" :class="{done: todo.done}">
+    <input type="checkbox" :checked="todo.done" @change="emit('toggle', todo.id)">
+    <span style="flex:1">{{ todo.text }}</span>
+    <button class="btn" @click="emit('remove', todo.id)">Eliminar</button>
+  </li>
+</template>
